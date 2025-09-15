edf8aae55e3399d4440410db26a76ec72d71f29f
Shashank Sharma <142104973+sha-nky@users.noreply.github.com>
2025-09-15 23:36:17 +0530

    Time: 0 ms (100%), Space: 12.3 MB (81.61%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,14 +1,26 @@
 class Solution(object):
     def climbStairs(self, n):
-        memo = {}
+        # 1st Solution :-
+        # memo = {}
         
-        def rec(n):
-            if n in [1, 2, 3]:
-                return n
-            if n in memo:
-                return memo[n]
+        # def rec(n):
+        #     if n in [1, 2, 3]:
+        #         return n
+        #     if n in memo:
+        #         return memo[n]
             
-            memo[n] = rec(n-1) + rec(n-2)
-            return memo[n]
+        #     memo[n] = rec(n-1) + rec(n-2)
+        #     return memo[n]
         
-        return rec(n)
+        # return rec(n)
+
+
+        # 2nd Solution :-
+        if n < 2:
+            return n
+        dp = [0] * (n + 1)
+        dp[1], dp[2] = 1, 2
+
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
