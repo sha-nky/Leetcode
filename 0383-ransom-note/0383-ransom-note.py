62fd62bcd5e2564d16238952cb6fda6ed893b08e
Shashank Sharma <142104973+sha-nky@users.noreply.github.com>
2025-09-15 22:28:10 +0530

    Time: 10 ms (90.47%), Space: 17.8 MB (96.78%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,25 +1,61 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        # 1st solution :-
+        # if len(ransomNote) > len(magazine):
+        #     return False
+        # f1 = {}
+        # f2 = {}
+
+        # for char in ransomNote:
+        #     if char not in f1:
+        #         f1[char] = 1
+        #     else:
+        #         f1[char] += 1
+        
+        # for char in magazine:
+        #     if char not in f2:
+        #         f2[char] = 1
+        #     else:
+        #         f2[char] += 1
+        
+        # uniq = set(ransomNote)
+
+        # for char in uniq:
+        #     if char not in f2 or f1[char] > f2[char]:
+        #         return False
+        # return True
+
+
+        # 2nd sloution :-
+        # if len(ransomNote) > len(magazine):
+        #     return False
+        
+        # freq = {}
+        # for char in ransomNote:
+        #     freq[char] = freq.get(char, 0) + 1
+        
+        # for char in magazine:
+        #     if char in freq and freq[char] > 0:
+        #         freq[char] -= 1
+        
+        # for value in freq.values():
+        #     if value > 0:
+        #         return False
+        # return True
+
+        # 3rd Solution :-
         if len(ransomNote) > len(magazine):
             return False
-        f1 = {}
-        f2 = {}
+        freq = {}
+        remaining = len(ransomNote)
 
         for char in ransomNote:
-            if char not in f1:
-                f1[char] = 1
-            else:
-                f1[char] += 1
-        
-        for char in magazine:
-            if char not in f2:
-                f2[char] = 1
-            else:
-                f2[char] += 1
-        
-        uniq = set(ransomNote)
+            freq[char] = freq.get(char, 0) + 1
 
-        for char in uniq:
-            if char not in f2 or f1[char] > f2[char]:
-                return False
-        return True
+        for char in magazine:
+            if char in freq and freq[char] > 0:
+                freq[char] -= 1
+                remaining -= 1
+                if remaining == 0:
+                    return True
+        return False
