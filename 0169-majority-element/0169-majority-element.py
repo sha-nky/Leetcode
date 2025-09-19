5a7489b7b25bf573f2454c26b11336ca6b18c018
Shashank Sharma <142104973+sha-nky@users.noreply.github.com>
2025-09-19 20:54:21 +0530

    Time: 16 ms (8.25%), Space: 19.3 MB (50.8%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,8 +1,14 @@
-from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        freq = Counter(nums)
-        
-        for num in freq.keys():
-            if freq.get(num) > (len(nums)//2):
-                return num
+        count = element = 0
+        for i in range(len(nums)):
+            if count == 0:
+                element = nums[i]
+                count = 1
+            elif nums[i] == element: count += 1
+            else: count -= 1
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] == element: count += 1
+        if count > len(nums)//2: return element
+        return -1
