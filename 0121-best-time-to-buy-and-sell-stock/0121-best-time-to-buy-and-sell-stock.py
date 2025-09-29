b9e2e677630ab5a3b61cbfcba9fe3f772566bf21
Shashank Sharma <142104973+sha-nky@users.noreply.github.com>
2025-09-30 00:10:14 +0530

    Time: 138 ms (16.27%), Space: 27 MB (32.75%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,10 +1,10 @@
-import math
-
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        minPrice = prices[0]
-        maxProfit = -math.inf
-        for i in range(len(prices)):
-            maxProfit = max(maxProfit, prices[i]-minPrice)
-            minPrice = min(minPrice, prices[i])
+        minPrice = float("inf")
+        maxProfit = 0
+
+        for price in prices:
+            minPrice = min(minPrice, price)
+            maxProfit = max(maxProfit, price - minPrice)
+        
         return maxProfit
