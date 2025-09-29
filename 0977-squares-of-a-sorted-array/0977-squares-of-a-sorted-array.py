517ab73fd55ae57d26e2b2eeb39ef0da244795f7
Shashank Sharma <142104973+sha-nky@users.noreply.github.com>
2025-09-30 01:51:27 +0530

    Time: 7 ms (79.59%), Space: 19.4 MB (76.16%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,32 +1,37 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        if not nums:
-            return []
+        # if not nums:
+        #     return []
         
-        pos = neg = []
-        point = 0
-        while point < len(nums) and nums[point] < 0:
-            point += 1
+        # pos = neg = []
+        # point = 0
+        # while point < len(nums) and nums[point] < 0:
+        #     point += 1
 
-        pos = nums[point:]
-        neg = nums[:point][::-1]
+        # pos = nums[point:]
+        # neg = nums[:point][::-1]
         
+        # res = []
+        # i, j = 0, 0
+        # while i < len(pos) and j < len(neg):
+        #     if pos[i] < abs(neg[j]):
+        #         res.append(pos[i] ** 2)
+        #         i += 1
+        #     else:
+        #         res.append(neg[j] ** 2)
+        #         j += 1
+        
+        # while i < len(pos):
+        #     res.append(pos[i] ** 2)
+        #     i += 1
+        
+        # while j < len(neg):
+        #     res.append(neg[j] ** 2)
+        #     j += 1
+        
+        # return res
+
         res = []
-        i, j = 0, 0
-        while i < len(pos) and j < len(neg):
-            if pos[i] < abs(neg[j]):
-                res.append(pos[i] ** 2)
-                i += 1
-            else:
-                res.append(neg[j] ** 2)
-                j += 1
-        
-        while i < len(pos):
-            res.append(pos[i] ** 2)
-            i += 1
-        
-        while j < len(neg):
-            res.append(neg[j] ** 2)
-            j += 1
-        
-        return res
+        for num in nums:
+            res.append(num ** 2)
+        return sorted(res)
