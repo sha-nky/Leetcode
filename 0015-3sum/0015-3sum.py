1a3c739b8c262a3e19870eeda7a9e761cf29c89a
Shashank Sharma <142104973+sha-nky@users.noreply.github.com>
2025-10-25 03:20:41 +0530

    Time: 487 ms (79.19%), Space: 20.6 MB (71.18%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,17 +3,30 @@ class Solution:
         nums.sort()
         
         def twoSum(numbers, i, j, target):
+            pairs = []
             while j > i:
-                if numbers[i] + numbers[j] == target:
-                    return [numbers[i], numbers[j]]
-                elif numbers[i] + numbers[j] > target:
+                s = numbers[i] + numbers[j]
+                if s == target:
+                    pairs.append([numbers[i], numbers[j]])
+                    i += 1
+                    j -= 1
+                    while i < j and numbers[i] == numbers[i-1]:
+                        i += 1
+                    while i < j and numbers[j] == numbers[j+1]:
+                        j -= 1
+                elif s > target:
                     j -= 1
                 else:
                     i += 1
-            return []
+            return pairs
         
         res = []
-        for i in range(0, len(nums)-3):
-            res.append([nums[i]] + twoSum(nums, i+1, len(nums)-1, -nums[i]))
+        for i in range(0, len(nums) - 2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            
+            pairs = twoSum(nums, i+1, len(nums)-1, -nums[i])
+            for p in pairs:
+                res.append([nums[i]] + p)
+        
         return res
-  
