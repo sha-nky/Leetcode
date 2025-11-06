542e6fd90fbb1eaf7f73d1b7877735b4eb091de7
Shashank Sharma <142104973+sha-nky@users.noreply.github.com>
2025-11-06 20:30:51 +0530

    Time: 3 ms (64.13%), Space: 19 MB (97.36%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,21 +1,42 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
+        # stack = []
+        # operators = "+-*/"
+        
+        # for token in tokens:
+        #     if token.lstrip("-").isdigit():
+        #         stack.append(int(token))
+        #     elif token in operators:
+        #         if len(stack) < 2:
+        #             return
+        #         arg2 = stack.pop()
+        #         arg1 = stack.pop()
+        #         if token == "/" and arg2 == 0:
+        #             return
+        #         res = int(eval(str(arg1) + token + str(arg2)))
+        #         stack.append(res)
+        #     else:
+        #         return
+        
+        # return stack.pop()
+
+
         stack = []
-        operators = "+-*/"
         
         for token in tokens:
-            if token.lstrip("-").isdigit():
+            if token not in {"+", "-", "*", "/"}:
                 stack.append(int(token))
-            elif token in operators:
-                if len(stack) < 2:
-                    return
-                arg2 = stack.pop()
-                arg1 = stack.pop()
-                if token == "/" and arg2 == 0:
-                    return
-                res = int(eval(str(arg1) + token + str(arg2)))
-                stack.append(res)
             else:
-                return
+                b = stack.pop()
+                a = stack.pop()
+                
+                if token == '+':
+                    stack.append(a + b)
+                elif token == '-':
+                    stack.append(a - b)
+                elif token == '*':
+                    stack.append(a * b)
+                else:
+                    stack.append(int(a / b))
         
         return stack.pop()
