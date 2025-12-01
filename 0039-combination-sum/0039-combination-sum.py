0802b8e53ea9019ede09fd7dc482fa3949fbda52
Shashank Sharma <142104973+sha-nky@users.noreply.github.com>
2025-12-01 15:26:35 +0530

    Time: 11 ms (57.87%), Space: 18 MB (39.12%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,18 +1,36 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
-        n = len(candidates)
+        # res = []
+        # n = len(candidates)
 
-        def dfs(i, k, temp):
-            if k==0:
-                res.append(temp[:])
-                return
-            if k<0:
-                return
-            for j in range(i, n):
-                temp.append(candidates[j])
-                dfs(j, k-candidates[j], temp)
-                temp.pop()
+        # def dfs(i, k, temp):
+        #     if k==0:
+        #         res.append(temp[:])
+        #         return
+        #     if k<0:
+        #         return
+        #     for j in range(i, n):
+        #         temp.append(candidates[j])
+        #         dfs(j, k-candidates[j], temp)
+        #         temp.pop()
         
-        dfs(0, target, [])
+        # dfs(0, target, [])
+        # return res
+
+
+        res = []
+
+        def rec(i, k, subset):
+            if k == 0:
+                res.append(subset[:])
+                return
+            if k < 0 or i == len(candidates):
+                return
+            
+            subset.append(candidates[i])
+            rec(i, k-candidates[i], subset)
+            subset.pop()
+            rec(i+1, k, subset)
+            
+        rec(0, target, [])
         return res
