1a86c6a77004700f28f5d4214dbad3cd08f7104d
Shashank Sharma <142104973+sha-nky@users.noreply.github.com>
2025-12-19 14:15:56 +0530

    Time: 1229 ms (1.08%), Space: 236.6 MB (12.09%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -6,11 +6,11 @@ class Solution:
         def rec(i, curAmt):
             if i > n-1:
                 return curAmt
-            if i in memo:
-                return memo[i]
+            if (i, curAmt) in memo:
+                return memo[(i, curAmt)]
             take = rec(i+2, curAmt + nums[i])
             notTake = rec(i+1, curAmt)
-            memo[i] = max(take, notTake)
-            return memo[i]
+            memo[(i, curAmt)] = max(take, notTake)
+            return memo[(i, curAmt)]
         
         return rec(0, 0)
