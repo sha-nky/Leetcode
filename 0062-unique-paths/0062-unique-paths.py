e555a18db69613f890d59c3dec0f0070c3d90f45
Shashank Sharma <142104973+sha-nky@users.noreply.github.com>
2025-12-28 17:57:19 +0530

    Time: 3 ms (23.12%), Space: 18.3 MB (8.59%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,4 +1,16 @@
-import math
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        return math.comb(m+n-2, m-1)
+        dp = {}
+
+        def dfs(i, j):
+            if i==m-1 and j==n-1:
+                return 1
+            if i>=m or j>=n:
+                return 0
+            if (i, j) in dp:
+                return dp[(i, j)]
+            
+            dp[(i, j)] = dfs(i+1, j) + dfs(i, j+1)
+            return dp[(i, j)]
+        
+        return dfs(0, 0)
