c8aaf9f40b42d828513e543246ad59341d74fa2a
Shashank Sharma <142104973+sha-nky@users.noreply.github.com>
2025-12-28 17:58:40 +0530

    Time: 0 ms (100%), Space: 17.3 MB (92.86%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,16 +1,30 @@
+import math
+
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = {}
+        # memo = {}
 
-        def dfs(i, j):
-            if i==m-1 and j==n-1:
-                return 1
-            if i>=m or j>=n:
-                return 0
-            if (i, j) in dp:
-                return dp[(i, j)]
+        # def rec(i, j):
+        #     if (i, j) in memo:
+        #         return memo[(i, j)]
+        #     if i == m-1 or j == n-1:
+        #         return 1
+        #     if i > m or j > n:
+        #         return 0
             
-            dp[(i, j)] = dfs(i+1, j) + dfs(i, j+1)
-            return dp[(i, j)]
+        #     memo[(i, j)] = rec(i+1, j) + rec(i, j+1)
+        #     return memo[(i, j)]
         
-        return dfs(0, 0)
+        # return rec(0, 0)
+
+
+        # dp = [[1] * n for _ in range(m)]
+
+        # for i in range(1, m):
+        #     for j in range(1, n):
+        #         dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        
+        # return dp[m-1][n-1]
+
+
+        return math.comb(m+n-2, m-1)
