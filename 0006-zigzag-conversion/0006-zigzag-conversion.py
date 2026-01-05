636c1cdf63bfd99e5f2412c5ba37b1648599dd4e
Shashank Sharma <142104973+sha-nky@users.noreply.github.com>
2026-01-05 23:44:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,20 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        pattern = [[] for _ in range(numRows)]
+        i = 0
+        direction = -1
+        
+        for ch in s:
+            pattern[i].append(ch)
+            if flag == -1 and i < numRows-1:
+                i += 1
+            elif flag == 1 and i > 0:
+                i -= 1
+            if i == 0 or i == numRows-1:
+                flag = -flag
+        
+        result = ""
+        for row in pattern:
+            result += ''.join(row)
+
+        return result
