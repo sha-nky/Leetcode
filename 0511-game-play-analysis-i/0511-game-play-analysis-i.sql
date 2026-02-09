a6978cbd5a11e7a710bb601fbbb8496d886e1ae6
Shashank Sharma <142104973+sha-nky@users.noreply.github.com>
2026-02-09 14:29:59 +0530

    Time: 488 ms (47.4%), Space: 0B (100%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -1,7 +1,11 @@
-select player_id, event_date as first_login
-from (
-    select player_id, event_date,
-        rank() over (partition by player_id order by event_date) as rnk
-    from activity
-) a
-where rnk = 1;
+-- select player_id, event_date as first_login
+-- from (
+--     select player_id, event_date,
+--         rank() over (partition by player_id order by event_date) as rnk
+--     from activity
+-- ) a
+-- where rnk = 1;
+
+select player_id, min(event_date) as first_login
+from activity
+group by player_id;
