f9ac60c0a5958e41b7fe33e84f1dd7741019ef86
Shashank Sharma <142104973+sha-nky@users.noreply.github.com>
2026-02-09 14:47:54 +0530

    Time: 415 ms (84.63%), Space: 0B (100%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,20 +1,29 @@
+-- -- select customer_number
+-- -- from orders
+-- -- group by customer_number
+-- -- order by count(order_number) desc
+-- -- limit 0, 1;
+
 -- select customer_number
--- from orders
--- group by customer_number
--- order by count(order_number) desc
--- limit 0, 1;
+-- from (
+--     select customer_number, count(*) as cnt
+--     from orders
+--     group by customer_number
+-- ) t
+-- where cnt = (
+--     select max(cnt)
+--     from (
+--         select count(*) as cnt
+--         from orders
+--         group by customer_number
+--     ) c
+-- );
 
 select customer_number
 from (
-    select customer_number, count(*) as cnt
+    select customer_number,
+        rank() over (order by count(*) desc) as rnk
     from orders
     group by customer_number
 ) t
-where cnt = (
-    select max(cnt)
-    from (
-        select count(*) as cnt
-        from orders
-        group by customer_number
-    ) c
-);
+where rnk = 1;
