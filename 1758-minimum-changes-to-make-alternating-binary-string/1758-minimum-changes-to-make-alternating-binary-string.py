f4432e1494420ef9ef7edc6434768a36149bd4df
Shashank Sharma <142104973+sha-nky@users.noreply.github.com>
2026-03-05 21:34:25 +0530

    Time: 5 ms (90.64%), Space: 19.3 MB (56.18%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
@@ -1,19 +1,28 @@
 class Solution:
     def minOperations(self, s: str) -> int:
-        n = len(s)
-        flag = 1
-        low, high = "", ""
-        for _ in range(n):
-            low += "0" if flag else "1"
-            high += "1" if flag else "0"
-            flag = not flag
+        # n = len(s)
+        # flag = 1
+        # low, high = "", ""
+        # for _ in range(n):
+        #     low += "0" if flag else "1"
+        #     high += "1" if flag else "0"
+        #     flag = not flag
         
-        if s == low or s == high:
-            return 0
+        # if s == low or s == high:
+        #     return 0
 
-        c1, c2 = 0, 0
-        for i in range(n):
-            c1 += 1 if s[i] != low[i] else 0
-            c2 += 1 if s[i] != high[i] else 0
+        # c1, c2 = 0, 0
+        # for i in range(n):
+        #     c1 += 1 if s[i] != low[i] else 0
+        #     c2 += 1 if s[i] != high[i] else 0
         
-        return min(c1, c2)
+        # return min(c1, c2)
+
+
+        diff = 0
+        n = len(s)
+        for i in range(n):
+            if s[i] != ("1" if i % 2 == 0 else "0"):
+                diff += 1
+        
+        return min(diff, n - diff)
