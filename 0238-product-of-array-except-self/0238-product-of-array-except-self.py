722abbbab71cc2651ff8fa71a68034615a447b2b
Shashank Sharma <142104973+sha-nky@users.noreply.github.com>
2026-03-24 14:52:54 +0530

    Time: 15 ms (94.07%), Space: 25.5 MB (76.23%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,23 +1,39 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prod = 1
-        count_zeroes = 0
-        for num in nums:
-            if num != 0:
-                prod *= num
-            else:
-                count_zeroes += 1
+        # prod = 1
+        # count_zeroes = 0
+        # for num in nums:
+        #     if num != 0:
+        #         prod *= num
+        #     else:
+        #         count_zeroes += 1
         
-        answer = []
-        for num in nums:
-            if count_zeroes > 1:
-                answer.append(0)
-            elif count_zeroes == 1:
-                if num != 0:
-                    answer.append(0)
-                else:
-                    answer.append(prod)
-            else:
-                answer.append(prod // num)
+        # answer = []
+        # for num in nums:
+        #     if count_zeroes > 1:
+        #         answer.append(0)
+        #     elif count_zeroes == 1:
+        #         if num != 0:
+        #             answer.append(0)
+        #         else:
+        #             answer.append(prod)
+        #     else:
+        #         answer.append(prod // num)
 
+        # return answer
+
+
+        n = len(nums)
+        answer = [1] * n
+
+        prefix = 1
+        for i in range(n):
+            answer[i] = prefix
+            prefix *= nums[i]
+        
+        suffix = 1
+        for i in range(n-1, -1, -1):
+            answer[i] *= suffix
+            suffix *= nums[i]
+        
         return answer
