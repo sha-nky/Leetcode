b3ff6505820fb4c2ffd63746897119594523b8c8
Shashank Sharma <142104973+sha-nky@users.noreply.github.com>
2026-03-29 13:50:04 +0530

    Time: 0 ms (100%), Space: 19.3 MB (74.85%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.py
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.py
@@ -1,17 +1,24 @@
 class Solution:
     def canBeEqual(self, s1: str, s2: str) -> bool:
-        m, n = len(s1), len(s2)
-        s1, s2 = list(s1), list(s2)
+        # m, n = len(s1), len(s2)
+        # s1, s2 = list(s1), list(s2)
 
-        for i in range(m-2):
-            s1[i], s1[i+2] = s1[i+2], s1[i]
+        # for i in range(m-2):
+        #     s1[i], s1[i+2] = s1[i+2], s1[i]
             
-            for j in range(n-2):
-                if ''.join(s1) == ''.join(s2):
-                    return True
-                s2[j], s2[j+2] = s2[j+2], s2[j]
+        #     for j in range(n-2):
+        #         if ''.join(s1) == ''.join(s2):
+        #             return True
+        #         s2[j], s2[j+2] = s2[j+2], s2[j]
             
-            if ''.join(s1) == ''.join(s2):
-                return True
+        #     if ''.join(s1) == ''.join(s2):
+        #         return True
         
-        return False
+        # return False
+
+
+        return (
+            sorted(s1[::2]) == sorted(s2[::2])
+            and
+            sorted(s1[1::2]) == sorted(s2[1::2])
+        )
