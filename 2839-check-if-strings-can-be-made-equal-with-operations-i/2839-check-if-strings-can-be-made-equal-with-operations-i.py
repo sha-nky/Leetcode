1d51b8773132a8749bff01490010d4ea064c0dcb
Shashank Sharma <142104973+sha-nky@users.noreply.github.com>
2026-03-29 13:46:50 +0530

    Time: 0 ms (100%), Space: 19.4 MB (15.79%) - LeetHub

---
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canBeEqual(self, s1: str, s2: str) -> bool:
+        m, n = len(s1), len(s2)
+        s1, s2 = list(s1), list(s2)
+
+        for i in range(m-2):
+            s1[i], s1[i+2] = s1[i+2], s1[i]
+            
+            for j in range(n-2):
+                if ''.join(s1) == ''.join(s2):
+                    return True
+                s2[j], s2[j+2] = s2[j+2], s2[j]
+            
+            if ''.join(s1) == ''.join(s2):
+                return True
+        
+        return False
