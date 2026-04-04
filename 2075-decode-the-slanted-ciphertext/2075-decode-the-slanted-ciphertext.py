e650f8ae10e93bba81cacdd27cc12a9455c8d9b2
Shashank Sharma <142104973+sha-nky@users.noreply.github.com>
2026-04-04 16:08:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.py
@@ -0,0 +1,23 @@
+class Solution:
+    def decodeCiphertext(self, encodedText: str, rows: int) -> str:
+        l = len(encodedText)
+        m, n = rows, l // rows
+        matrix = [[" "] * n for _ in range(m)]
+        
+        i, j = 0, 0
+        for k in range(l):
+            matrix[i][j] = encodedText[k]
+            j += 1
+            if j == n:
+                i += 1
+                j = 0
+        
+        decodedText = ""
+        for col in range(n):
+            i, j = 0, col
+            while i < m and j < n:
+                decodedText += matrix[i][j]
+                i += 1
+                j += 1
+        
+        return decodedText.strip()
