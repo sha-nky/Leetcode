062fbec833435794cb7ab2273faca36669df8905
Shashank Sharma <142104973+sha-nky@users.noreply.github.com>
2026-04-04 16:11:13 +0530

    Time: 0 ms (100%), Space: 26.2 MB (100%) - LeetHub

--- 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.py
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.py
@@ -1,23 +1,42 @@
 class Solution:
     def decodeCiphertext(self, encodedText: str, rows: int) -> str:
+        # l = len(encodedText)
+        # m, n = rows, l // rows
+        # matrix = [[" "] * n for _ in range(m)]
+        
+        # i, j = 0, 0
+        # for k in range(l):
+        #     matrix[i][j] = encodedText[k]
+        #     j += 1
+        #     if j == n:
+        #         i += 1
+        #         j = 0
+        
+        # decodedText = ""
+        # for col in range(n):
+        #     i, j = 0, col
+        #     while i < m and j < n:
+        #         decodedText += matrix[i][j]
+        #         i += 1
+        #         j += 1
+        
+        # return decodedText.rstrip()
+
+
+        if rows == 1:
+            return encodedText
+
         l = len(encodedText)
-        m, n = rows, l // rows
-        matrix = [[" "] * n for _ in range(m)]
-        
-        i, j = 0, 0
-        for k in range(l):
-            matrix[i][j] = encodedText[k]
-            j += 1
-            if j == n:
-                i += 1
-                j = 0
-        
+        cols = l // rows
+        i, j, k = 0, 0, 0
         decodedText = ""
-        for col in range(n):
-            i, j = 0, col
-            while i < m and j < n:
-                decodedText += matrix[i][j]
-                i += 1
+
+        while k < l:
+            decodedText += encodedText[k]
+            i += 1
+            if i == rows:
+                i = 0
                 j += 1
-        
+            k = i*(cols + 1) + j
+
         return decodedText.rstrip()
