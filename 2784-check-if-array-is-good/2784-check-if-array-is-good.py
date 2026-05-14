1da7648d0abf7d18926787ac35408ca8566574cf
Shashank Sharma <142104973+sha-nky@users.noreply.github.com>
2026-05-14 14:11:40 +0530

    Time: 1 ms (39.15%), Space: 19.2 MB (89.68%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.py
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.py
@@ -13,7 +13,20 @@ class Solution:
         # return True
 
 
-        nums.sort()
-        n = len(nums)
 
-        return nums == list(range(1, n)) + [n-1]
+        # nums.sort()
+        # n = len(nums)
+
+        # return nums == list(range(1, n)) + [n-1]
+
+
+
+        nums.sort()
+        max_ele = nums[-1]
+        if len(nums) != max_ele + 1 or nums[-1] != nums[-1-1]:
+            return False
+        
+        for i in range(max_ele - 1):
+            if nums[i] != i + 1:
+                return False
+        return True
