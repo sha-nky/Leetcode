5ca3aceb4a852044e12f377ff295fac967d1fceb
Shashank Sharma <142104973+sha-nky@users.noreply.github.com>
2026-06-17 14:38:06 +0530

    Time: 4 ms (76.47%), Space: 23 MB (98.69%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.py
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.py
@@ -3,7 +3,7 @@ class Solution:
         result = ""
         
         for char in s:
-            if char == "*" and s:
+            if char == "*":
                 result = result[:-1]
             elif char == "%":
                 result = result[::-1]
