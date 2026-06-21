5a2913bef7fbdbd903afd33859e8e07523545131
Shashank Sharma <142104973+sha-nky@users.noreply.github.com>
2026-06-21 19:55:06 +0530

    Time: 101 ms (14.11%), Space: 31.1 MB (76.9%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
@@ -14,6 +14,6 @@ class Solution:
                 iceCreams += 1
                 coins -= coin
             else:
-                break
+                return iceCreams
         
         return iceCreams
