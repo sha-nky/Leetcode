29d298cd3ba858467988ca54da3d3df14a7b2f74
Shashank Sharma <142104973+sha-nky@users.noreply.github.com>
2026-06-21 19:58:35 +0530

    Time: 35 ms (97.36%), Space: 30.9 MB (90.23%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
@@ -1,19 +1,40 @@
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
-        counts = [0] * (max(costs) + 1)
+        # counts = [0] * (max(costs) + 1)
+        # for cost in costs:
+        #     counts[cost] += 1
+        
+        # sortedCoins = []
+        # for count in range(len(counts)):
+        #     sortedCoins.extend([count] * counts[count])
+        
+        # iceCreams = 0
+        # for coin in sortedCoins:
+        #     if coin <= coins:
+        #         iceCreams += 1
+        #         coins -= coin
+        #     else:
+        #         return iceCreams
+        
+        # return iceCreams
+
+
+        maxCost = max(costs)
+        counts = [0] * (maxCost + 1)
         for cost in costs:
             counts[cost] += 1
         
-        sortedCoins = []
-        for count in range(len(counts)):
-            sortedCoins.extend([count] * counts[count])
-        
         iceCreams = 0
-        for coin in sortedCoins:
-            if coin <= coins:
-                iceCreams += 1
-                coins -= coin
-            else:
-                return iceCreams
-        
+        for cost in range(1, maxCost + 1):
+            if counts[cost] == 0:
+                continue
+
+            canBuy = min(counts[cost], coins // cost)
+
+            iceCreams += canBuy
+            coins -= canBuy * cost
+
+            if coins < cost:
+                break
+
         return iceCreams
