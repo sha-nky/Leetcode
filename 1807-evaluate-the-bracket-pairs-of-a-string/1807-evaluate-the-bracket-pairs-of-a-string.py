e4e823f3376f0817bb22ad8e2673e2ab0cf7ff59
Shashank Sharma <142104973+sha-nky@users.noreply.github.com>
2026-09-26 11:16:28 +0530

    Time: 37 ms (91.28%), Space: 52 MB (21.81%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.py
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.py
@@ -1,23 +1,40 @@
 class Solution:
     def evaluate(self, s: str, knowledge: list[list[str]]) -> str:
-        knowledge = {key: value for key, value in knowledge}
+        # knowledge = {key: value for key, value in knowledge}
         
-        res = []
-        n = len(s)
-        flag = 0
+        # res = []
+        # n = len(s)
+        # flag = 0
 
-        for i in range(n):
-            if s[i] == "(":
-                flag = 1
-                key = ""
-            elif s[i] == ")":
-                flag = 0
-                res.append(knowledge.get(key, "?"))
-                key = ""
-            else:
-                if flag:
-                    key += s[i]
-                else:
-                    res.append(s[i])
+        # for i in range(n):
+        #     if s[i] == "(":
+        #         flag = 1
+        #         key = ""
+        #     elif s[i] == ")":
+        #         flag = 0
+        #         res.append(knowledge.get(key, "?"))
+        #         key = ""
+        #     else:
+        #         if flag:
+        #             key += s[i]
+        #         else:
+        #             res.append(s[i])
         
-        return "".join(res)
+        # return "".join(res)
+
+
+        knowledge = dict(knowledge)
+        result = []
+
+        i = 0
+        while i < len(s):
+            if s[i] == "(":
+                j = s.index(")", i)
+                key = s[i+1:j]
+                result.append(knowledge.get(key, "?"))
+                i = j+1
+            else:
+                result.append(s[i])
+                i += 1
+
+        return "".join(result)
